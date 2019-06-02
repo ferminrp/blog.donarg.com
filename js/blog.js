39c4fe9c001f85e9ed20c45bc6beb7e6ff21106a
ferminrp <frodriguezpenelas@gmail.com>
2019-06-02 07:45:59 -0300

    no message

--- js/blog.js
+++ js/blog.js
@@ -4,7 +4,8 @@ url = 'https://api.sheety.co/80ce190e-9a4f-47d2-a2a6-d71381da3e01'
 
 //connecting to spreadsheet and then parsing the data
   $.getJSON(url, function(data) {
+    //var obj = data.feed.entry;
     var obj = data;
-    console.log(obj[1]);
-    console.log(obj.slice(0, 3));
-   });
+    var first_title = obj[0].titulo;
+    console.log(first_title)
+  });
