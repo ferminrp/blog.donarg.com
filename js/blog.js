52c061739ba3d38b878951393a1337e9bc5d76fc
ferminrp <frodriguezpenelas@gmail.com>
2019-06-02 07:26:52 -0300

    otro js test

--- js/blog.js
+++ js/blog.js
@@ -13,9 +13,9 @@ url = 'https://api.sheety.co/80ce190e-9a4f-47d2-a2a6-d71381da3e01'
       var paginaWeb = this.paginaWeb
       var instagram = this.instagram
       var twitter = this.twitter
-      var linkDonaciones = this.linkDonaciones*/
+      var linkDonaciones = this.linkDonaciones
 
       console.log(this.title)
-      $(".featured").append('<div class="ngo-card"> <a href="'+ paginaWeb +'" target="_blank" rel="noopener"><div id="featured-two" class="img-container" style="background-image:url('+imagen+')!important"></div> </a><p>'+ong+'</p><div class="icon-list"> <span><a href="'+paginaWeb+'" target="_blank" rel="noopener"><i class="fas fa-globe"></i></a></span> <span><a href="'+instagram+'" target="_blank" rel="noopener"><i class="fab fa-instagram"></i></a></span> <span><a href="'+twitter+'" target="_blank" rel="noopener"><i class="fab fa-twitter"></i></a></span></div></div>')
+      $(".featured").append('<div class="ngo-card"> <a href="'+ paginaWeb +'" target="_blank" rel="noopener"><div id="featured-two" class="img-container" style="background-image:url('+imagen+')!important"></div> </a><p>'+ong+'</p><div class="icon-list"> <span><a href="'+paginaWeb+'" target="_blank" rel="noopener"><i class="fas fa-globe"></i></a></span> <span><a href="'+instagram+'" target="_blank" rel="noopener"><i class="fab fa-instagram"></i></a></span> <span><a href="'+twitter+'" target="_blank" rel="noopener"><i class="fab fa-twitter"></i></a></span></div></div>')*/
    });
   });
