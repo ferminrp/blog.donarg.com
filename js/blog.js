a46211ea19ac740bfedcd3e4bb0fc6df6bbef6ff
ferminrp <frodriguezpenelas@gmail.com>
2019-06-02 15:19:55 -0300

    Supuestamente esto lo soluciona

--- js/blog.js
+++ js/blog.js
@@ -13,6 +13,6 @@ url = 'https://api.sheety.co/80ce190e-9a4f-47d2-a2a6-d71381da3e01'
       var link = this.link;
 
       console.log(this.title);
-      $(".blogposts").append('<a href="'+link+'"><div class="post grow"><div class="post-text"><h6>'+title+'</h6> <p>'+description+'</p></div><div class="post-image" style="background-image:url('imagen')"> </div></div></a>');
+      $(".blogposts").append('<a href="'+link+'"><div class="post grow"><div class="post-text"><h6>'+title+'</h6> <p>'+description+'</p></div><div class="post-image" style="background-image:url('+imagen+')"> </div></div></a>');
    });
   });
