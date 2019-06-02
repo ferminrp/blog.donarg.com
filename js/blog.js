5e7661f68a34bf8e3b8903d9e6eceacbe1339bc0
ferminrp <frodriguezpenelas@gmail.com>
2019-06-02 15:18:15 -0300

    Probando problemas con el js

--- js/blog.js
+++ js/blog.js
@@ -7,12 +7,12 @@ url = 'https://api.sheety.co/80ce190e-9a4f-47d2-a2a6-d71381da3e01'
     //var obj = data.feed.entry;
     var obj = data;
     $(obj).each(function(){
-      var title = this.titulo
-      var imagen = this.imagen
-      var description = this.descripcion
-      var link = this.link
+      var title = this.titulo;
+      var imagen = this.imagen;
+      var description = this.descripcion;
+      var link = this.link;
 
-      console.log(this.title)
-      $(".blogposts").append('<a href="'+link+'"><div class="post grow"><div class="post-text"><h6>'+title+'</h6> <p>'+description+'</p></div><div class="post-image" style="background-image:url('imagen')"> </div></div></a>')
+      console.log(this.title);
+      $(".blogposts").append('<a href="'+link+'"><div class="post grow"><div class="post-text"><h6>'+title+'</h6> <p>'+description+'</p></div><div class="post-image" style="background-image:url('imagen')"> </div></div></a>');
    });
   });
