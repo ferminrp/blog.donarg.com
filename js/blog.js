402513428c11aaadc5d29c215f2608f15ebbac65
ferminrp <frodriguezpenelas@gmail.com>
2019-06-02 07:59:37 -0300

    no message

--- js/blog.js
+++ js/blog.js
@@ -7,6 +7,7 @@ url = 'https://api.sheety.co/80ce190e-9a4f-47d2-a2a6-d71381da3e01'
     //var obj = data.feed.entry;
     var obj = data;
     var tres_notas = obj.slice(1,4);
+    var item;
     for (var item in tres_notas) {
       console.log(item.titulo);
     }
